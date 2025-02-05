b30a3c5955254442980ab0c94d4d313f31ba761f
atalaymurat <atalaymurat@msn.com>
2025-02-05 11:54:18 +0300

    nginx

--- express/server.js
+++ express/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const mongoose = require("mongoose")
 
 // Database connection
@@ -27,6 +26,6 @@ app.use((req, res, next) => {
   res.status(404).send("404 not Found!")
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`)
 })
