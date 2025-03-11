2e6b734d00d80b9bd1b9067561914e0c5b7b3b22
atalaymurat <atalaymurat@msn.com>
2025-03-11 18:09:41 +0300

    basic passport auth ok 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import axios from "axios"
 
 
-// axios.defaults.baseURL = "https://express-server-s0sj.onrender.com"
-axios.defaults.baseURL = "http://localhost"
+axios.defaults.baseURL = "https://express-server-s0sj.onrender.com"
+// axios.defaults.baseURL = "http://localhost"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
