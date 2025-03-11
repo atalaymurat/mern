6ae7c355a6e087cb542d1482c98025e1fa6936fb
atalaymurat <atalaymurat@msn.com>
2025-03-11 15:58:54 +0300

    ..

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import axios from "axios"
 
 
-// axios.defaults.baseURL = "https://express-server-s0sj.onrender.com"
-axios.defaults.baseURL = "http://localhost"
+axios.defaults.baseURL = "https://express-server-s0sj.onrender.com"
+// axios.defaults.baseURL = "http://localhost"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
