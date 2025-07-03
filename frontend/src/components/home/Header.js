94dc3eda903279e73f00fab954c3d3978cccd7d2
atalaymurat <atalaymurat@msn.com>
2025-07-03 21:23:35 +0300

    updates

--- frontend/src/components/home/Header.js
+++ frontend/src/components/home/Header.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <div className="my-2 border-t border-b p-4 bg-slate-900">
-                <h1 className="text-lg md:text-6xl text-center font-bold text-white">
-                    <span className="text-xs mx-4 font-light">[tech]</span>
-                    DOC MANAGER
-                </h1>
-            </div>
+  return (
+    <div>
+      <div className="my-2 border-t border-b p-2 bg-slate-100 flex flex-row items-center justify-center">
+        <img
+          src="/varolLogo.png"
+          className="mx-2 mr-auto"
+          style={{
+            height: "130px",
+            width: "auto",
+            objectFit: "contain",
+          }}
+        />
+        <div className="text-sm text-center font-semibold text-gray-600 pr-4">
+          Ufuk Varol
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
